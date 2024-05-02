dd1bd252dfa62b4835efbb30c6f89019641dd37d
Justin Dang <jdang065@bolt.cs.ucr.edu>
2024-05-01 18:11:41 -0700

    testing reddit praw

--- crawl.py
+++ crawl.py
@@ -1,11 +1,22 @@
-import requests
 import praw
-from bs4 import BeautifulSoup
-seed = "https://en.wikipedia.org/wiki/Randomness"
+# import requests
+# from bs4 import BeautifulSoup
+# seed = "https://en.wikipedia.org/wiki/Randomness"
 
-page = requests.get(seed)
-soup = BeautifulSoup(page.content, "html.parser")
+# page = requests.get(seed)
+# soup = BeautifulSoup(page.content, "html.parser")
 
-for link in soup.find_all('a'):
-    # do whatever with links here
-    print("hi")
+# for link in soup.find_all('a'):
+#     # do whatever with links here
+#     print("hi")
+
+ID          = "7UI5BZd_IpRM-Opi3WtSOA"
+SECRET      = "mw-ViF2wBo6gEwE_PuB4kQHermrjjg"
+AGENT       = "cs172"
+
+reddit = praw.Reddit(
+    client_id=ID,
+    client_secret=SECRET,
+    user_agent=AGENT
+)
+
