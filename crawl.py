a4c1d8b1d9c56863f947e644d81ece24943e8897
Justin Dang <jdang065@bolt.cs.ucr.edu>
2024-05-02 15:54:35 -0700

    misc

--- crawl.py
+++ crawl.py
@@ -30,7 +30,7 @@ seed = "Helldivers"
 
 def crawlSubreddit(subreddit):
     postCount = 1
-    for post in reddit.subreddit(subreddit).top('hour', limit = postLimit):
+    for post in reddit.subreddit(subreddit).top(time_filter = 'hour', limit = postLimit):
         # grab dictionary with attributes of object using vars()
         dict = vars(post)
         print(f"Parsing: ({post.title})[{postCount}:{postLimit}]")
@@ -61,7 +61,7 @@ def crawlSubreddit(subreddit):
 
 def crawlRedditor(redditor):
     # Grab new subreddits visited here as well as item essentials
-    for post in reddit.redditor(redditor).submissions.top('hour', limit = postLimit):
+    for post in reddit.redditor(redditor).submissions.top(timer_filter = 'hour', limit = postLimit):
         # grab dictionary with attributes of object using vars()
         dict = vars(post)
         print(f"Parsing: ({post.title})[{postCount}:{postLimit}]\n")
