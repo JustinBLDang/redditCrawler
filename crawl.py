14d11696e68fbbbe3b517f9f1143d6a138f08364
Justin Dang <jdang065@bolt.cs.ucr.edu>
2024-05-05 16:38:44 -0700

    bug fixes

--- crawl.py
+++ crawl.py
@@ -1,4 +1,5 @@
 import praw
+import prawcore
 import json
 import sys
 import queue
@@ -110,51 +111,54 @@ def crawlRedditor(redditor):
     print(f"-----------------------------------------------\nCrawling {redditor}\n")
     # Grab new subreddits visited here as well as item essentials
     for post in reddit.redditor(redditor).submissions.top(limit = postLimit):
-        # try to avoid error 429
-        if(postCount % numPostsPerSleep == 0):
-            time.sleep(sleepTime)
-        
-        # ignore posts we already crawled
-        if(post.title in crawledPosts or post is None):
-            print("Dupe post or none existent: ")
+        try:
+            # try to avoid error 429
+            if(postCount % numPostsPerSleep == 0):
+                time.sleep(sleepTime)
+            
+            # ignore posts we already crawled
+            if(post.title in crawledPosts or post is None):
+                print("Dupe post or none existent: ")
+                postCount += 1
+                continue
+
+            if(postCount > postLimit):
+                print(f"Skipping {redditor}, reached search limit.\n")
+                return
+            
+            # Add post to dupe check
+            crawledPosts.add(post.title)
+
+            # grab dictionary with attributes of object using vars()
+            dict = vars(post)
+            print(f"Parsing: ({post.title})[{postCount}:{postLimit}]\n")
+
+            # grab specific attributes specified in fields, written above, for current post. 
+            sub_dict = {field:dict[field] for field in fields}
+
+            # Feed crawler subreddits, 
+            if(post.subreddit.name not in crawledSubreddit):
+                subReddit.put(post.subreddit.name)
+
+            # grab all comments for the current post
+            comments = []
+            post.comments.replace_more(limit=commentLimit, threshold=commentThreshold)
+
+            # Helper counter for comments
+            commentCount = 1
+            for comment in post.comments.list():
+                print("\r", end='')
+                print(f"Downloading Comments: {commentCount}", end='', flush = True)     
+                sys.stdout.flush()  
+                comments.append(comment.body)
+                commentCount += 1
+            sub_dict['comments'] = comments
+
+            # Add post to items, which will be stored later in json
+            items.append(sub_dict)
             postCount += 1
+        except prawcore.exceptions.Redirect:
             continue
-
-        if(postCount > postLimit):
-            print(f"Skipping {redditor}, reached search limit.\n")
-            return
-        
-        # Add post to dupe check
-        crawledPosts.add(post.title)
-
-        # grab dictionary with attributes of object using vars()
-        dict = vars(post)
-        print(f"Parsing: ({post.title})[{postCount}:{postLimit}]\n")
-
-        # grab specific attributes specified in fields, written above, for current post. 
-        sub_dict = {field:dict[field] for field in fields}
-
-        # Feed crawler subreddits, 
-        if(post.subreddit.name not in crawledSubreddit):
-            subReddit.put(post.subreddit.name)
-
-        # grab all comments for the current post
-        comments = []
-        post.comments.replace_more(limit=commentLimit, threshold=commentThreshold)
-
-        # Helper counter for comments
-        commentCount = 1
-        for comment in post.comments.list():
-            print("\r", end='')
-            print(f"Downloading Comments: {commentCount}", end='', flush = True)     
-            sys.stdout.flush()  
-            comments.append(comment.body)
-            commentCount += 1
-        sub_dict['comments'] = comments
-
-        # Add post to items, which will be stored later in json
-        items.append(sub_dict)
-        postCount += 1
         
     # add redditor to crawled redditors
     crawledUsers.add(redditor)
@@ -171,17 +175,20 @@ def main():
         if(subReddit.empty() and users.empty()):
             print("------------------------------------------------------------------------\nCould not finish, unable to find unique users and subreddits\n")
             break
-        crawlSubreddit(subReddit.get())
-        
+        subRedditName = subReddit.get()
+        crawlSubreddit(subRedditName)
+
         # ensure we get subreddits for next iteration
         while(subReddit.empty()):
             crawlRedditor(users.get())
 
         json_str = json.dumps(items, sort_keys=True, indent=4)
+        
+        #write json_str to crawl.json
+        with open(f'{subRedditName}.json', 'w') as f:
+            json.dump(items, f)
 
-    #write json_str to crawl.json
-    with open('crawl.json', 'w') as f:
-        json.dump(items, f)
+    
     
     # Output Subreddits for double checking dupes
     print(crawledSubreddit)
