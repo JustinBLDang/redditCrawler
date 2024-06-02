dffb3dabd3a153c2e1625775873ea91c94ae454c
Justin Dang <jdang065@bolt.cs.ucr.edu>
2024-06-01 18:09:28 -0700

    part 2

--- sample.py
+++ sample.py
@@ -1,19 +1,20 @@
 import re
 import json
+
 import logging, sys
 logging.disable(sys.maxsize)
 
+import lucene
 import os
-from pathlib import Path
-# from java.nio.file import Paths
-
+from org.apache.lucene.store import MMapDirectory, SimpleFSDirectory, NIOFSDirectory
+from java.nio.file import Paths
 from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.document import Document, Field, FieldType
-from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig, IndexOptions, DirectoryReader
-from org.apache.lucene.store import MMapDirectory, SimpleFSDirectory, NIOFSDirectory
 from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig, IndexOptions, DirectoryReader
 from org.apache.lucene.search import IndexSearcher, BoostQuery, Query
 from org.apache.lucene.search.similarities import BM25Similarity
+
 
 def create_index(dir):
     if not os.path.exists(dir):
